5431bcf3fd5a147b91b1e2b6dd00b828682885c4
Jean Joskin <jean@joskin.nl>
2011-04-12 13:50:40 +0200

    Add: convenience package Language.JsLib

---
+++ src/Language/JsLib.hs
@@ -0,0 +1,11 @@
+module Language.JsLib
+( module Language.JsLib.AST
+, module Language.JsLib.Scanner
+, module Language.JsLib.Parser
+, module Language.JsLib.Printer
+) where
+
+import Language.JsLib.AST
+import Language.JsLib.Scanner
+import Language.JsLib.Parser
+import Language.JsLib.Printer
